9677f6284bcd1b83a26565e3d0258e7ae74d2bdf
RainMolisa <60537623+RainMolisa@users.noreply.github.com>
2020-03-04 18:09:16 +0800

    1

---
+++ jcd7_0/downSample.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<vector>
+#include<opencv.hpp>
+namespace dsp
+{
+	float* dwnsp01(float* depth,int rows,int cols)
+	{
+		int r2 = rows / 2;
+		int c2 = cols / 2;
+		for (int y = 0; y < r2; y++)
+		{
+			for (int x = 0; x < c2; x++)
+			{
+
+			}
+		}
+	}
+	float* dwnsp02(float* depth, int rows, int cols)
+	{
+
+	}
+	float* dwnsp03(float* depth, int rows, int cols)
+	{
+
+	}
+};
