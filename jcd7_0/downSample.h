7c176b8ce5f43dc54bf5155884d4e21957c4ea26
RainMolisa <60537623+RainMolisa@users.noreply.github.com>
2020-03-04 22:09:36 +0800

    ds

--- jcd7_0/downSample.h
+++ jcd7_0/downSample.h
@@ -11,16 +11,53 @@ namespace dsp
 		{
 			for (int x = 0; x < c2; x++)
 			{
-
+				cv::Point p1(2 * x, 2 * y);
+				cv::Point p2(2 * x + 1, 2 * y);
+				cv::Point p3(2 * x, 2 * y + 1);
+				cv::Point p4(2 * x + 1, 2 * y + 1);
+				float v1 = depth[p1.y * cols + p1.x];
+				float v2 = depth[p2.y * cols + p2.x];
+				float v3 = depth[p3.y * cols + p3.x];
+				float v4 = depth[p4.y * cols + p4.x];
 			}
 		}
 	}
 	float* dwnsp02(float* depth, int rows, int cols)
 	{
-
+		int r2 = rows / 2;
+		int c2 = cols / 2;
+		for (int y = 0; y < r2; y++)
+		{
+			for (int x = 0; x < c2; x++)
+			{
+				cv::Point p1(2 * x, 2 * y);
+				cv::Point p2(2 * x + 1, 2 * y);
+				cv::Point p3(2 * x, 2 * y + 1);
+				cv::Point p4(2 * x + 1, 2 * y + 1);
+				float v1 = depth[p1.y * cols + p1.x];
+				float v2 = depth[p2.y * cols + p2.x];
+				float v3 = depth[p3.y * cols + p3.x];
+				float v4 = depth[p4.y * cols + p4.x];
+			}
+		}
 	}
 	float* dwnsp03(float* depth, int rows, int cols)
 	{
-
+		int r2 = rows / 2;
+		int c2 = cols / 2;
+		for (int y = 0; y < r2; y++)
+		{
+			for (int x = 0; x < c2; x++)
+			{
+				cv::Point p1(2 * x, 2 * y);
+				cv::Point p2(2 * x + 1, 2 * y);
+				cv::Point p3(2 * x, 2 * y + 1);
+				cv::Point p4(2 * x + 1, 2 * y + 1);
+				float v1 = depth[p1.y * cols + p1.x];
+				float v2 = depth[p2.y * cols + p2.x];
+				float v3 = depth[p3.y * cols + p3.x];
+				float v4 = depth[p4.y * cols + p4.x];
+			}
+		}
 	}
 };
